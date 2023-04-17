4dbf570897ca673ba13fa5a3a45405611b8486be
zxc3buttons <tokare2002@gmail.com>
2023-04-17 22:36:06 +0300

    Initial commit

---
+++ database/migrations/2023_04_17_192337_create_notebooks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notebooks', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name', 250);
+            $table->string('company', 150)->nullable();
+            $table->string('phone', 20);
+            $table->string('email', 50);
+            $table->date('birth_date')->nullable();
+            $table->text('image_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notebooks');
+    }
+};
